2ff0571df814556380c48614236ece063eb1edf3
juyoung518 <55094564+juyoung518@users.noreply.github.com>
2020-04-24 10:22:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ dataBlockSize = dataBlock.calculateDataBlockSizeInWords(evalboard.getNumEnabledD
 
 # Select Number of Datablocks to read to achieve objective RefreshRate
 # 20000hz -> 20000 samples per sec. 1 sample per 1/20000sec.
-usbBlocksToRead = ceil(getBoardSampleRate() / (rhd2kbd.SAMPLES_PER_DATA_BLOCK * getObjectiveRefreshRate()))
+usbBlocksToRead = int(ceil(getBoardSampleRate() / (rhd2kbd.SAMPLES_PER_DATA_BLOCK * getObjectiveRefreshRate())))
 timePerReadingSessionIncr = rhd2kbd.SAMPLES_PER_DATA_BLOCK / getBoardSampleRate() * usbBlocksToRead
 
 # Keep Track of Elapsed Time(s)
